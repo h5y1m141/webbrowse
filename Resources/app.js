5cd0912905a1b6eb3bc30329907ead80c6310228
hiroshi oyamada <h5y1m141+github@gmail.com>
2011-11-17 07:21:25 +0900

    URL入力して画面表示できる機能実装
    URL入力してエンターで確定した後にイベントが発火される。Indicator表示→URLとして入力された文字列をwebview.urlに渡す→w
    ebview.reload()にてページ取得

--- Resources/app.js
+++ Resources/app.js
@@ -16,8 +16,57 @@ var tab1 = Titanium.UI.createTab({
     title:'Tab 1',
     window:win1
 });
+/*
+ URLを入力するテキストボックスを作成
+ URL入力して内容確定した時にイベント発火させて
+ wevbiewのURLプロパティに値を渡す
+*/
+var urlBox = Titanium.UI.createTextField({
+  color:'#336699',
+  top:10,
+  left:10,
+  width:200,
+  height:40,
+  hintText:'enter url',
+  keyboardType:Titanium.UI.KEYBOARD_URL,
+  returnKeyType:Titanium.UI.RETURNKEY_DEFAULT,
+  autocapitalization: false,
+  borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
+});
+
+urlBox.addEventListener('blur',function(e){
+  Ti.API.info(actInd);
+  actInd.show();
+  webView.url = e.value;
+  webView.reload();
+
+});
+win1.add(urlBox);
+var actInd = Titanium.UI.createActivityIndicator({
+  top:50,
+  height:55,
+  width:'auto',
+  color:'#FFFFFF',
+  backgroundColor:'#000',
+  opacity:0.5,
+  borderRadius:5,
+  borderColor:'#000',
+  font:{fontFamily:'Helvetica Neue', fontSize:13},
+  message:' Loading...',
+  style:Titanium.UI.iPhone.ActivityIndicatorStyle.PLAIN
+});
+win1.add(actInd);
+
+
 var webView = Titanium.UI.createWebView({
-	url:'http://www.google.co.jp'
+  top:50,
+  left:0,
+  width:'auto',
+  height:'auto'
+});
+
+webView.addEventListener('load',function(e){
+  actInd.hide();
 });
 win1.add(webView);
 
