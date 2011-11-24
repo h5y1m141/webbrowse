1240ec891027dd167a6213a7be8fe2d36c63077b
hiroshi oyamada <h5y1m141+github@gmail.com>
2011-11-24 21:04:56 +0900

    webViewにHTMLコンテンツ埋め込むものを実装

--- Resources/app.js
+++ Resources/app.js
@@ -9,73 +9,19 @@ var tab1 = Titanium.UI.createTab({
     title:'Tab 1',
     window:win1
 });
-
-// １．URLを入力できるテキストフィールドを準備
-var urlBox = Titanium.UI.createTextField({
-  color:'#336699',
-  top:10,
-  left:10,
-  width:200,
-  height:30,
-  hintText:'enter url',
-  keyboardType:Titanium.UI.KEYBOARD_URL,
-  returnKeyType:Titanium.UI.RETURNKEY_DEFAULT,
-  autocapitalization: false,
-  borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
-});
-urlBox.addEventListener('blur',function(e){
-  actInd.show();
-  webView.url = e.value;
-  webView.reload();
-
-});
-win1.add(urlBox);
-
-// ２．読み込み処理を中止するボタンを準備
-var stopBtn = Ti.UI.createButton({
-  title:'stop',
-  top:10,
-  left:220,
-  width:60,
-  height:30,
-  systemButton:Titanium.UI.iPhone.SystemButton.STOP
-});
-stopBtn.addEventListener('click',function(e){
-  webView.stopLoading();
-  actInd.hide();
-});
-win1.add(stopBtn);
-
-// ３．HTMLコンテンツを表示するためのWebViewを準備
+/
 var webView = Titanium.UI.createWebView({
-  top:50,
-  left:0,
-  width:'auto',
-  height:'auto'
 });
-webView.addEventListener('beforeload',function(e){
-  actInd.show();
-});
-webView.addEventListener('load',function(e){
-  actInd.hide();
-});
+webView.html =
+  '<html>' +
+    '<head>' +
+      '<title>自分で準備したHTMLを表示</title>' +
+    '</head>' +
+    '<body>' +
+      '<h1>自分でつくったHTMLコンテンツ表示する</h1>' +
+      '<p>こんな感じでコンテンツが表示できます</p>' +
+    '</body>' +
+  '</html>';
 win1.add(webView);
-
-// ４．「読み込み中」ということを示すためのActivityIndicatorを準備
-var actInd = Titanium.UI.createActivityIndicator({
-  top:50,
-  height:55,
-  width:'auto',
-  opacity:0.5,
-  color:'#FFFFFF',
-  backgroundColor:'#000',
-  borderRadius:5,
-  borderColor:'#000',
-  font:{fontFamily:'Helvetica Neue', fontSize:13},
-  message:' Loading...',
-  style:Titanium.UI.iPhone.ActivityIndicatorStyle.PLAIN
-});
-win1.add(actInd);
-
 tabGroup.addTab(tab1);
 tabGroup.open();
