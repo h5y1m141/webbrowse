9d735d1074204a9f6d8c89568faccafe38cccb68
hiroshi oyamada <h5y1m141+github@gmail.com>
2011-12-01 09:31:24 +0900

    webViewのHTMLプロパティを利用したサンプル

--- Resources/app.js
+++ Resources/app.js
@@ -1,7 +1,8 @@
+
 Titanium.UI.setBackgroundColor('#FFF');
 var tabGroup = Titanium.UI.createTabGroup();
 var win1 = Titanium.UI.createWindow({
-   title:'簡易ブラウザ',
+   title:'自分で準備したHTMLを表示',
    backgroundColor:'#fff'
 });
 var tab1 = Titanium.UI.createTab({
@@ -23,5 +24,30 @@ webView.html =
     '</body>' +
   '</html>';
 win1.add(webView);
+
+// create controls tab and root window
+//
+var win2 = Titanium.UI.createWindow({
+    title:'Tab 2',
+    backgroundColor:'#fff'
+});
+var tab2 = Titanium.UI.createTab({
+    icon:'KS_nav_ui.png',
+    title:'Tab 2',
+    window:win2
+});
+var label2 = Titanium.UI.createLabel({
+	color:'#999',
+	text:'I am Window 2',
+	font:{fontSize:20,fontFamily:'Helvetica Neue'},
+	textAlign:'center',
+	width:'auto'
+});
+win2.add(label2);
+//
+//  add tabs
+//
 tabGroup.addTab(tab1);
+tabGroup.addTab(tab2);
+// open tab group
 tabGroup.open();
